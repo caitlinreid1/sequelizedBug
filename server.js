f4ed47d2fe76d980cf1b4cd5a400f7accace14ab
C. Reid <ctlnrd@gmail.com>
2017-05-23 16:25:47 -0400

    routes edit

--- server.js
+++ server.js
@@ -21,8 +21,10 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/bugsController.js");
+// Routes =============================================================
+require("./routes/author-api-routes.js")(app);
+require("./routes/html-routes.js")(app);
+require("./routes-/post-api-routes.js")(app);
 
 app.use("/", routes);
 
